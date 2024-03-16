1404d55029a4a07e0d670b53f2ae80ac163f23fa
Alexey Volniokv <v0lnik0v33@gmail.com>
2024-03-16 14:35:45 +0300

    Initial Commit

---
+++ HW32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW32
+//
+//  Created by Алексей Вольников on 16.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
